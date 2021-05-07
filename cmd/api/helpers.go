644f15e40d3af979d1b220ae483ace9d6f257ccf
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-05-07 14:22:22 +0800

    Wrap JSON data in a parent level JSON envelope

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,14 +9,17 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// Define an envelope type
+type envelope map[string]interface{}
+
 // Retrieve the "id" URL parameter from the current request context, then convert it to
 // an integer and return it.
 // The integer is base10 with type int (machine-dependent bit size).
 // If the conversion is not successful, return 0 and an error.
-func (app *application) readIDParam(r *http.Request) (int, error) {
+func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
-	id, err := strconv.Atoi(params.ByName("id"))
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
@@ -27,9 +30,9 @@ func (app *application) readIDParam(r *http.Request) (int, error) {
 // Define a writeJSON() helper for sending responses. This takes the destination
 // http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a 
 // header map containing any additional HTTP headers we want to include in the response.
-func (app * application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+func (app * application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	// Encode the data to JSON, return error if any.
-	js, err := json.Marshal(data)
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
