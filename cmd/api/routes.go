32c2712ebc6e6a07828993b7b74d2813a7693031
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-05-07 14:56:20 +0800

    Use custom handlers in application

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,6 +13,15 @@ func (app *application) routes() *httprouter.Router {
 	// https://pkg.go.dev/github.com/julienschmidt/httprouter#Router
 	router := httprouter.New()
 
+	// Convert the notFoundResponse() helper to a http.Handler using the 
+	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
+	// Not Found responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	// Convert the methodNotAllowed() helper to a http.Handler and set it as a custom error
+	// handler for 405 Method Not Allowed responses.
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// Register the relevant methods, URL patterns and handler functions for the endpoints
 	// using the HandlerFunc() method.
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
