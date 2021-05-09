ce2617bbe8491f5133475bea15b0e2663d1c876f
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-05-09 11:26:47 +0800

    Add bash commands to test create movie validation

--- bash/createmovie.sh
+++ bash/createmovie.sh
@@ -1,27 +1,40 @@
+INPUTMESSAGE="***** Request Body *****"
 OUTPUTMESSAGE="***** Response *****"
 
 # Should give json.SyntaxError
 echo Sending XML as request body...
+echo "$INPUTMESSAGE"
+BODY='<?xml version="1.0" encoding="UTF-8"?><note><to>Alex</to></note>'
+echo "$BODY"
 echo "$OUTPUTMESSAGE"
-curl -i -d '<?xml version="1.0" encoding="UTF-8"?><note><to>Alex</to></note>' localhost:4000/v1/movies
+curl -i -d "$BODY" localhost:4000/v1/movies
 echo
 
 # Should give json.SyntaxError
 echo Sending malformed JSON with trailing comma...
+echo "$INPUTMESSAGE"
+BODY='{"title": "Moana", }'
+echo "$BODY"
 echo "$OUTPUTMESSAGE"
-curl -i -d '{"title": "Moana", }' localhost:4000/v1/movies
+curl -i -d "$BODY" localhost:4000/v1/movies
 echo
 
 # Should give json.UnmarshalTypeError
 echo Sending an JSON array instead of an object...
+echo "$INPUTMESSAGE"
+BODY='["foo", "bar"]'
+echo "$BODY"
 echo "$OUTPUTMESSAGE"
-curl -i -d '["foo", "bar"]' localhost:4000/v1/movies
+curl -i -d "$BODY" localhost:4000/v1/movies
 echo
 
 # Should give json.UnmarshalTypeError
 echo Sending a numeric \"title\" value instead of a string...
+echo "$INPUTMESSAGE"
+BODY='{"title": 123}'
+echo "$BODY"
 echo "$OUTPUTMESSAGE"
-curl -i -d '{"title": 123}' localhost:4000/v1/movies
+curl -i -d "$BODY" localhost:4000/v1/movies
 echo
 
 # Should give io.EOF
@@ -31,21 +44,30 @@ curl -i -X POST localhost:4000/v1/movies
 echo
 
 # Should give "json: unknown field"
-echo Sending JSON with unknown key \"rating\"
+echo Sending JSON with unknown key \"rating\"...
+echo "$INPUTMESSAGE"
+BODY='{"title": "Moana", "rating": "PG"}'
+echo "$BODY"
 echo "$OUTPUTMESSAGE"
-curl -i -d '{"title": "Moana", "rating": "PG"}' localhost:4000/v1/movies
+curl -i -d "$BODY" localhost:4000/v1/movies
 echo
 
 # Multiple JSON values instead of one
 echo Sending two consecutive JSON data...
+echo "$INPUTMESSAGE"
+BODY='{"title": "Moana"}{"title": "Top Gun"}'
+echo "$BODY"
 echo "$OUTPUTMESSAGE"
-curl -i -d '{"title": "Moana"}{"title": "Top Gun"}' localhost:4000/v1/movies
+curl -i -d "$BODY" localhost:4000/v1/movies
 echo
 
 # Body contains garbage content after the first JSON value
 echo Sending JSON with garbage value after...
+echo "$INPUTMESSAGE"
+BODY='{"title": "Moana"} :~()'
+echo "$BODY"
 echo "$OUTPUTMESSAGE"
-curl -i -d '{"title": "Moana"} :~()' localhost:4000/v1/movies
+curl -i -d "$BODY" localhost:4000/v1/movies
 echo
 
 # Send a 1.5MB JSON file
@@ -57,4 +79,22 @@ if [ ! -f "$FILE" ]; then
 fi
 echo "$OUTPUTMESSAGE"
 curl -i -d @/tmp/largefile.json localhost:4000/v1/movies
+echo
+
+# Test validation logic of handler
+echo Sending a JSON with invalid data...
+echo "$INPUTMESSAGE"
+BODY='{"title":"","year":1000,"runtime":"-123 mins","genres":["sci-fi","sci-fi"]}'
+echo "$BODY"
+echo "$OUTPUTMESSAGE"
+curl -i -d "$BODY" localhost:4000/v1/movies
+echo
+
+# Valid request
+echo Sending a JSON with valid data...
+echo "$INPUTMESSAGE"
+BODY='{"title":"Moana","year":2016,"runtime":"107 mins","genres":["animation","adventure"]}'
+echo "$BODY"
+echo "$OUTPUTMESSAGE"
+curl -i -d "$BODY" localhost:4000/v1/movies
 echo
