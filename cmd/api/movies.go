a1e4b1429ef94500d61ceeff7381759b3928a227
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-05-16 15:55:18 +0800

    Add handlers to show, update and delete movies

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/jseow5177/greenlight/internal/data"
 	"github.com/jseow5177/greenlight/internal/validator"
@@ -75,13 +75,17 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := &data.Movie{
-		ID: id,
-		CreatedAt: time.Now(),
-		Title: "Casablance",
-		Runtime: 102,
-		Genres: []string{"drama", "romance", "war"},
-		Version: 1,
+	// Check if error returned is data.ErrRecordNotFound
+	// If yes, return a 404 Not Found response to the client
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
@@ -89,5 +93,98 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
+	}
+}
+
+// Add a updateMovieHandler for "PUT /v1/movies/:id"
+func (app *application) updateMovieHandler (w http.ResponseWriter, r *http.Request) {
+	// Extract the movie ID from URL
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// Fetch the existing movie record from the database
+	// Send a 404 Not Found response to the client if we couldn't find a matching record
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Declare an input struct to hold the expected data from the client
+	var input struct {
+		Title string `json:"title"`
+		Year int32 `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres []string `json:"genres"`
+	}
+
+	// Read the JSON request body data into the input struct
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the request body to the appropriate fields of the movie record
+	movie.Title = input.Title
+	movie.Year = input.Year
+	movie.Runtime = input.Runtime
+	movie.Genres = input.Genres
+
+	// Validate the updated movie, sending the client a 422 Unprocessable Entity if any checks fail
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Pass the updated movie to the Update() method
+	err = app.models.Movies.Update(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// Write the updated movie into JSON response
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+}
+
+// Add a deleteMovieHandler for "DELETE /v1/movies/:id"
+func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract movie ID from URL
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	err = app.models.Movies.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Return a 200 OK status code along with status message
+	// Optionally, can send a 204 No Content with an empty response body
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "movie successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
 	}
 }
