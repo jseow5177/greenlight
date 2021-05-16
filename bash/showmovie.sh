372cc1d866dc31b5fe63a7338d34f389f7312caf
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-05-16 14:36:45 +0800

    Add scripts to test get movie handler

---
+++ bash/showmovie.sh
@@ -0,0 +1,22 @@
+OUTPUTMESSAGE="***** Response *****"
+
+# Get an existing movie
+# Remember to run createmovie.sh first
+echo Get movie of id 1
+echo "$OUTPUTMESSAGE"
+curl -i localhost:4000/v1/movies/1
+echo
+
+# Get non-existent movie
+echo Get movie of id 999999
+echo "$OUTPUTMESSAGE"
+curl -i localhost:4000/v1/movies/999999
+echo
+
+# Get movie of negative id
+echo Get movie of id -1
+echo Get movie of id 999999
+echo "$OUTPUTMESSAGE"
+curl -i localhost:4000/v1/movies/-1
+echo
+
