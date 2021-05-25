5e94a02344c7101b90b4a55ba785e11054ee8dfb
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-05-25 09:39:33 +0800

    Add helpers to form ORDER BY clause while preventing SQL injection

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,40 @@
 package data
 
-import "github.com/jseow5177/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/jseow5177/greenlight/internal/validator"
+)
+
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafeList []string
+}
+
+// Check that the client-provided Sort field matche one of the entries in our safelist.
+// If it does, extract the column name from the Sort field by stripping the leading hyphen character (if it exists).
+// sortColumn() is constructed to panic if the client-provided Sort value does not match one of the entries in the safelist.
+// Technically, this should not happen because the Sort value would have been checked by ValidateFilters().
+// But it is a sensible failsafe to stop SQL injection.
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe soft parameter: " + f.Sort)
+}
+
+// Return the sort direction ("ASC" or "DESC") depending on the prefix character of the Sort field.
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
