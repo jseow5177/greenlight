975515da0d97682357233ca801e28dbc556032a1
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-05-31 17:29:27 +0800

    Add scripts to test registerUserHandler

---
+++ bash/registeruser.sh
@@ -0,0 +1,29 @@
+INPUTMESSAGE="***** Request Body *****"
+OUTPUTMESSAGE="***** Response *****"
+
+# Register a new user with valid credentials
+echo Registering a new user with valid credentials...
+echo "$INPUTMESSAGE"
+BODY='{"name": "Alice Smith", "email": "alice@example.com"}'
+echo "$BODY"
+echo "$OUTPUTMESSAGE"
+curl -i -d "$BODY" localhost:4000/v1/users
+echo
+
+# Register a new user with invalid credentials
+echo Registering a new user with invalid credentials...
+echo "$INPUTMESSAGE"
+BODY='{"name": "", "email": "alice@invalid.", "password": "123"}'
+echo "$BODY"
+echo "$OUTPUTMESSAGE"
+curl -i -d "$BODY" localhost:4000/v1/users
+echo
+
+# Register a new user with valid credentials but duplicate email
+echo Registering a new user with valid credentials but duplicate email...
+echo "$INPUTMESSAGE"
+BODY='{"name": "Alice Drinkwater", "email": "ALICE@example.com", "password": "pa55word"}'
+echo "$BODY"
+echo "$OUTPUTMESSAGE"
+curl -i -d "$BODY" localhost:4000/v1/users
+echo
