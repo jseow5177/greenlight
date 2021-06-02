8f938121b281b7236e908bb3ddc56cd72ced44b2
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-06-02 16:04:01 +0800

    Add comments

--- migrations/000004_create_users_table.up.sql
+++ migrations/000004_create_users_table.up.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS users (
   id bigserial PRIMARY KEY,
   created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
   name text NOT NULL,
-  email citext UNIQUE NOT NULL, -- case-insensitive text
+  -- Stores text data exactly as inputted, but comparisons are case-insensitive.
+  -- Combined with citext type, the UNIQUE constraint means that no two rows can have the same value - even if different cases.
+  email citext UNIQUE NOT NULL,
   password_hash bytea NOT NULL, -- binary string type column
   activated bool NOT NULL, -- To denote whether a user account is active
   version integer NOT NULL DEFAULT 1
