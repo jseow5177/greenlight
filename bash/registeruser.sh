b292661cf5b918b99ce65ae62d59c801dd7baaaa
Jonathan Seow <jseow006@e.ntu.edu.sg>
2021-06-02 16:06:14 +0800

    Fix scripts

--- bash/registeruser.sh
+++ bash/registeruser.sh
@@ -4,10 +4,19 @@ OUTPUTMESSAGE="***** Response *****"
 # Register a new user with valid credentials
 echo Registering a new user with valid credentials...
 echo "$INPUTMESSAGE"
-BODY='{"name": "Alice Smith", "email": "alice@example.com"}'
+BODY='{"name": "Alice Smith", "email": "ALICE@example.com", "password": "pa55word"}'
 echo "$BODY"
 echo "$OUTPUTMESSAGE"
-curl -i -d "$BODY" localhost:4000/v1/users
+curl -w '\nTime: %{time_total}\n' -i -d "$BODY" localhost:4000/v1/users
+echo
+
+# Register a new user with valid credentials but duplicate email
+echo Registering a new user with valid credentials but duplicate email...
+echo "$INPUTMESSAGE"
+BODY='{"name": "Alice Smith", "email": "alice@example.com", "password": "pa55word"}'
+echo "$BODY"
+echo "$OUTPUTMESSAGE"
+curl -w '\nTime: %{time_total}\n' -i -d "$BODY" localhost:4000/v1/users
 echo
 
 # Register a new user with invalid credentials
@@ -16,14 +25,5 @@ echo "$INPUTMESSAGE"
 BODY='{"name": "", "email": "alice@invalid.", "password": "123"}'
 echo "$BODY"
 echo "$OUTPUTMESSAGE"
-curl -i -d "$BODY" localhost:4000/v1/users
-echo
-
-# Register a new user with valid credentials but duplicate email
-echo Registering a new user with valid credentials but duplicate email...
-echo "$INPUTMESSAGE"
-BODY='{"name": "Alice Drinkwater", "email": "ALICE@example.com", "password": "pa55word"}'
-echo "$BODY"
-echo "$OUTPUTMESSAGE"
-curl -i -d "$BODY" localhost:4000/v1/users
+curl -w '\nTime: %{time_total}\n' -i -d "$BODY" localhost:4000/v1/users
 echo
